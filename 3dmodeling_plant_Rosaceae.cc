5d32e4a62488b8bbc651eb9557c6afb4391cd900
panguojun <panguojun@users.noreply.github.com>
2021-08-19 11:32:19 +0800

    Update 3dmodeling_plant_Rosaceae.cc

--- 3dmodeling_plant_Rosaceae.cc
+++ 3dmodeling_plant_Rosaceae.cc
@@ -173,20 +173,162 @@ void rose(vec o, vec v0, real sz, VECLIST e, int depth)
 }
 
 // --------------------------------------------------------
-// 最简代码设计测试
+// 山茶花叶子
 // --------------------------------------------------------
-#define loopi(len)	int __len = len; for(int i=0; i<__len; i++)
-#define __ai		(i / __len)
-#define PI2		(PI * 2.0f)
-
-void flower()
+void leafunit(VECLIST& e, real fw0, crvec v, vec up, real sz, int len, real fang, real fang2, real ang3, real pm1, real pm2, real pm3, real pm4)
 {
-	EDGE e;
-	loopi{
-		real ang = __ai * PI2;
-		real r = 1.0f;
-		vec2 p = vec2(cos(ang), sin(ang)) * r;
-		e = e + p;
+	gcommonvertex = 1;
+	vec forward = v;
+	vec right = up.cross(forward).normcopy();
+	up = forward.cross(right).normcopy();
+
+	float w = fw0 / 2 * clamp(0.5, 4, 12, 5);
+	VECLIST le = e;
+	for (int ii = 0; ii <= len; ii++)
+	{
+		real aii = ii / real(len);
+		if (g_leafunit_yemai)
+			color = blendcor(RGB(100, 150, 100), gcurcor, 0.5 + 0.5 * sin((len / 2)* PI*aii));
+		else
+			color = blendcor(RGB(150, 180, 150), gcurcor, aii, 0.5);
+		if (ii < len * 0.4)
+			SETPARM(10);
+		else if (ii > len * 0.6)
+			SETPARM(11);
+		else
+			SETPARM(12);
+		{
+			real step[] = { 15 * USIZE, 15 * USIZE, 15 * USIZE, 15 * USIZE, 15 * USIZE };
+
+			real fw = fw0 * (1 - aii) + w * blend(1, pm3, aii, pm1) * blend3(0.1, pm4, aii, 0.35, pm2); //blend(pm4, 0.5, aii, pm2);
+
+			forward.rot(fang * (1 - aii), right);
+			up.rot(fang2, forward);
+			right = up.cross(forward).normcopy();
+
+			real ang = blend(-ang3, ang3, aii);
+			vec left1 = right.rotcopy(-ang, up);
+			vec right1 = right.rotcopy(ang, up);
+
+			up = forward.cross(right).normcopy();
+
+			vec p = e[2].p;
+			e.clear();
+
+			e.push_back(p + forward * step[0] - left1 * (fw)+v * (fw));
+			e.push_back(p + forward * step[1] - left1 * (fw / 4) + v * (fw * lerp(0.8, -0.25, aii)) + up * (-fw / 8));
+			e.push_back(p + forward * step[2]);
+			e.push_back(p + forward * step[3] + right1 * (fw / 4) + v * (fw * lerp(0.8, -0.25, aii)) + up * (-fw / 8));
+			e.push_back(p + forward * step[4] + right1 * (fw)+v * (fw));
+		}
+		{
+			int color0 = color;
+			for (int i = 0; i < int(le.size() - 1); i++)
+			{
+				color = color0;
+				if (i == le.size() / 2)
+					color = RGB(100, 150, 100);
+				plane(le[i], le[i + 1], e[i + 1], e[i]);
+			}
+		}
+		le = e;
 	}
-	branch(e);
+	gcommonvertex = 0;
+}
+void leafunit(vec o, vec v, real sz, int len, real fang, real fang2, real ang3, real pm1, real pm2, real pm3, real pm4)
+{
+	vec forward = v;
+	vec up = vec::UY;
+	vec right = -up.cross(forward).normcopy();
+	up = forward.cross(right).normcopy();
+
+	VECLIST e;
+	float w = sz / 10;
+	e.push_back(o - right * (w));
+	e.push_back(o - right * (w / 4) + up * (w / 4));
+	e.push_back(o);
+	e.push_back(o + right * (w / 4) + up * (w / 4));
+	e.push_back(o + right * w);
+
+	leafunit(e, w, v, up, sz, len, fang, fang2, ang3, pm1, pm2, pm3, pm4);
+}
+
+// --------------------------------------------------------------------
+void shanchaleave(vec o, vec uy = vec::UY, real s = 0.1, int len0 = 20, int depth = 0)
+{
+	vec p = o;
+	VECLIST e;
+	color = 0xff101010;
+	roundedge(e, o - vec::UY * (s), uy, s, depth == 0 ? 5 : 3, 0);
+	bool lstgcommonvertex = gcommonvertex;
+	gcommonvertex = 1;
+	int cnt = 0;
+	int cnt1 = 0;
+	real dpdeta = rrnd(0, 172.862);
+	real dpdeta2 = rrnd(0, 172.862);
+	{// jing
+		int len = depth == 0 ? clamp(3, 20, 10, 3) * 3 : len0;
+		for (int i = 0; i < len; i++)
+		{
+			real ai = real(i) / (len);
+			real t = blend(0.0, 1, ai);
+			vec dp = vec::UX * ((perlin(p.x * 0.75, p.y * 0.05 + dpdeta, p.z * 0.3))) + vec::UZ * ((perlin(p.x * 0.2, p.y * 0.025 + dpdeta2, p.z * 0.5)));
+			vec np = p;
+			if (depth == 0)
+				np += (uy + dp) * (USIZE * blend(50, 20, ai));
+			else
+				np += (uy + dp * 0.5) * (USIZE * 55);
+			{
+				color = blendcor(0xFF102000, 0xFFFFFFFF, ai, 2);
+				SETPARM(10);
+				VECLIST e1;
+				{
+					vec dv = np - p;
+					moveedge(e, dv, e1);
+					scaleedge(e1, 0.99);
+				}
+				face(e, e1);
+				e = e1;
+			}
+			if (depth < 4 && i > 0.15 * len && ((depth == 0 && rand() % 5 == 0) || (depth == 1 && rand() % 10 == 0) || (depth == 2 && rand() % 30 == 0) || (depth == 3 && rand() % 25 == 0)))
+			{
+				vec pp = getedgecenter(e);
+				if (depth == 0)
+				{
+					vec v = (np - p).normcopy();
+					v = blend(v, vec::UX.rotcpy(cnt * PI / 2, vec::UY), blend(0.6, 0.25, (ai - 0.5) / 0.5));
+					cnt++;
+					//shanchaleave(pp, v, s * rrnd(0.4, 0.6), MIN(len, 10) / 2, depth + 1);
+					shanchaleave(pp, v, s * rrnd(0.4, 0.6), MIN(len * rrnd(0.25, 1.5), 10), depth + 1);
+				}
+				else
+				{
+					for (int ii = 0; ii < 4; ii++)
+					{
+						vec v = (np - p).normcopy();
+						v = blend(v, vec::UX.rotcpy(cnt * PI / 2, vec::UY), blend(0.6, 0.25, (ai - 0.5) / 0.5));
+						cnt++;
+						shanchaleave(pp, v, s * rrnd(0.4, 0.6), len * blend(1.0, 0.25, ai), depth + 1);
+					}
+				}
+			}
+			else if (i == len - 1)
+			{
+				vec v = (np - p).normcopy();
+				real ang = rrnd(-PI, PI) / 20;
+				vec pp = getedgecenter(e);
+				for (int ii = 0; ii < 1; ii++)
+				{
+					v.rot(ang + PI * ii, uy);
+					//g_leafunit_yemai = 1;
+					leafunit(pp, v, 0.5, clamp(3, 30, 11, 2), rrnd(-0.1, 0.1), rrnd(-0.1, 0.1), rrnd(-0.1, 0.1),
+						1.1, 2.02,
+						1.15, 8);
+					//g_leafunit_yemai = 0;
+				}
+			}
+			p = np;
+		}
+	}
+	gcommonvertex = lstgcommonvertex;
 }
