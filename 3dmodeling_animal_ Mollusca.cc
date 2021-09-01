5239b26a32c2ca7d830eefb33e0b86b23e341e2e
panguojun <panguojun@users.noreply.github.com>
2021-09-01 09:57:21 +0800

    Update 3dmodeling_animal_ Mollusca.cc

--- 3dmodeling_animal_ Mollusca.cc
+++ 3dmodeling_animal_ Mollusca.cc
@@ -5,30 +5,30 @@
 // Gastropoda
 // ------------------------------------------------
 void shell()
-	{
-		comv(true);
-		rgb(200, 200, 150);
+{
+	comv(true);
+	rgb(200, 200, 150);
 
-		edge e;
-		loopi(64) {
-			real ang = __ai * PI;
-			real r = 12.0f;
-			vec p = vec(cos(ang), 0, sin(ang)) * r;
-			p.x *= blend(1., 8., __ai, 18);
+	edge e;
+	loopi(64) {
+		real ang = __ai * PI;
+		real r = 12.0f;
+		vec p = vec(cos(ang), 0, sin(ang)) * r;
+		p.x *= blend(1., 8., __ai, 18);
 
-			e = e | p;
-		}
-
-		begintke(e);
-		trunk(0, 158,
-			trunk_cb(d){
-				rot(blend(10, 15, d / 58.), vec3::UX);
-				mov(vec3::UX * blend(0.25, 0.15, d / 158.));
-				scl(0.97);
-				ext((d % 2 == 1 ? 1 : 0.25) * blend(1, 0.25, d / 158.) * 0.05);
-				face(d % 2);
-			}
-		);
-		endtk;
+		e = e | p;
 	}
-	
+
+	begintke(e);
+	trunk(0, 158,
+		trunk_cb(d){
+			rot(blend(10, 15, d / 58.), vec3::UX);
+			mov(vec3::UX * blend(0.25, 0.15, d / 158.));
+			scl(0.97);
+			ext((d % 2 == 1 ? 1 : 0.25) * blend(1, 0.25, d / 158.) * 0.05);
+			face(d % 2);
+		}
+	);
+	endtk;
+}
+
