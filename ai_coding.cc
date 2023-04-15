2fd9b23daf2123af742e16c25cb533a8bd49b995
panguojun <panguojun@users.noreply.github.com>
2023-04-15 01:59:49 +0000

    Update ai_coding.cc

--- ai_coding.cc
+++ ai_coding.cc
@@ -1,6 +1,51 @@
-以下是一个绘制海带的例子：
+float rndmap(int x, int y)
+{
+    int n = x + y * 57;
+    n = (n << 13) ^ n;
+    return (1.0 - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0);
+}
+float beizer(float x, float c[4])
+{
+    float t = x;
+    float u = 1.0 - t;
+    float tt = t * t;
+    float uu = u * u;
+    float uuu = uu * u;
+    float ttt = tt * t;
 
+    float res = uuu * c[0]; // 计算曲线上的点
+    res += 3.0 * uu * t * c[1];
+    res += 3.0 * u * tt * c[2];
+    res += ttt * c[3];
+    return res;
+}
 void haidai(vec2 p, int len)
+{
+    static real s_t1[4] = { rrnd(0, 1), rrnd(0, 1), rrnd(0, 1), rrnd(0, 1) }; // 随机数数组
+
+    for (int i = 0; i < len; i++) // 循环绘制每个点
+    {
+        real ai = real(i) / len; // 计算当前点在曲线上的位置
+        p = p + vec2(beizer(ai * PI * 2 + gtime * 1.1, s_t1) * 0.002, 0.004); // 计算当前点的位置
+        if (i % 8 < 6) // 每隔6个点绘制一个像素
+        {
+            pixel(p);
+        }
+        if (i % 20 == 0) // 每隔20个点绘制一个海带叶片
+        {
+            vec2 pp = p; // 复制当前点位置
+            vec2 vv(rndmap2(0,i), 0.1 + rndmap(0,i)); vv.norm(); // 计算随机向量
+            for (int ii = 0; ii < 12; ii++) // 循环绘制叶片的12个点
+            {
+                pp = pp + vv * 0.002; // 计算叶片的下一个点位置
+                pixel(pp); // 绘制叶片的一个点
+                vv = vv.rot(PI / 6); // 旋转随机向量
+            }
+        }
+    }
+}
+
+void haidai0(vec2 p, int len)
 {
     static float s_t1[4] = { rrnd(0, 1), rrnd(0, 1), rrnd(0, 1), rrnd(0, 1) }; // 随机数数组
 
